1a681bb0dc76424fd42f81c034ff09e0e24b96a4
anjali-0404 <tripathianjali760@gmail.com>
2026-06-24 22:15:34 +0530

    updated server.js

--- backend/server.js
+++ backend/server.js
@@ -1,11 +1,11 @@
 const express = require("express");
-const mongoose = require("mongoose");
 const cors = require("cors");
 const dotenv = require("dotenv");
 dotenv.config();
 const helmet = require("helmet");
 const morgan = require("morgan");
 const passport = require("./config/passport");
+
 
 // Route Imports
 const connectDB = require("./config/db");
